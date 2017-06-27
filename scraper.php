ed918e24f1502c4e09d73929cfdce593607cfb17
John Handelaar <john@handelaar.org>
2017-06-27 10:05:00 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,22 +1,107 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+// Ireland PlanningAlerts scraper for Cork City Council
+// Uses LGMA shared backend for map data at ArcGIS.com
+//
+// John Handelaar 2017-06-27
 
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
+date_default_timezone_set('Europe/Dublin');
+$date_format = 'Y-m-d';
+$cookie_file = '/tmp/cookies.txt';
+
+
+# Splitting this for legibility only
 $remote_uri = 'https://services3.arcgis.com/vgpNvkwrqKit2cbA/arcgis/rest/services/NationalPlanningApplications_Points' . 
               '/FeatureServer/0/query?f=json&where=((PlanningAuthority%20%3D%20%27Cork%20City%20Council%27)' .
               '%20AND%20(ReceivedDate%20BETWEEN%20CURRENT_TIMESTAMP%20-%2030%20AND%20CURRENT_TIMESTAMP))%20AND%20(1%3D1)' .
               '&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&' .
               'outSR=4326&resultOffset=0&resultRecordCount=500';
 
-$applications = json_decode(scraperwiki::scrape($remote_uri));
+$council_eplan_root_uri = 'http://planning.corkcity.ie/';
+$council_comment_url = "http://www.corkcity.ie/services/strategicplanningeconomicdevelopment/planningapplicationsprocess/makingasubmissiononaplanningapplication/";
+
+/*
+ ------------------------------------------------------------
+ You should not need to alter this scraper below this point.
+ ------------------------------------------------------------
+ */
+
+
+# Get the file contents as JSON
+$curl = curl_init($remote_uri);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; PlanningAlerts/0.1; +http://www.planningalerts.org.au/)");
+$json_response = curl_exec($curl);
+curl_close($curl);
+
+
+# Get a page from the Cork City planning portal and discard it
+# Otherwise first attempt to retrieve application by URI will be redirected to home
+$curl = curl_init($council_eplan_root_uri);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_file);
+curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_file);
+$got_cookie = curl_exec($curl);
+curl_close($curl);
+unset($got_cookie);
+
+
+# Decode and process the data
+$applications = json_decode($json_response);
 
 foreach ($applications->features as $application) {
-  echo $application->attributes->PlanningAuthority . ": " . $application->attributes->ApplicationNumber ."\n";
+    echo 'Found ' . $application->attributes->PlanningAuthority . ': ' . $application->attributes->ApplicationNumber . '\n';
+    $council_reference = $application->attributes->ApplicationNumber;
+    $info_url = $application->attributes->LinkAppDetails;
+    $comment_url = $council_comment_url;
+    $lat = $application->attributes->geometry->y;
+    $lng = $application->attributes->geometry->x;
+    $date_scraped = date($date_format);
+    
+    # Retrieve this application
+    $curl = curl_init($application->attributes->LinkAppDetails);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_file);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_file);
+    $appdetails = curl_exec($curl);
+    curl_close($curl);
+    
+    $parsethis = new simple_html_dom();
+    $parsethis->load($appdetails);
+    
+    $addresspath = $parsethis->find('#Development table',0)->find('tr',2)->find('td',0);
+    $address = trim($addresspath->plaintext);
+
+    $descriptionpath = $parsethis->find('#Development table',0)->find('tr',1)->find('td',0);
+    $description = trim($descriptionpath->plaintext);
+
+    $receivedpath = $parsethis->find('#Details table',0)->find('tr',4)->find('td',0);
+    $date_received = date($date_format,strtotime(str_replace('/','-',trim($receivedpath->plaintext)))); # Don't use slashes -- US dateformat assumed if you do
+    $on_notice_from = $date_received;
+    
+    $deadlinepath = $parsethis->find('#Details table',0)->find('tr',9)->find('td',1);
+    $on_notice_to = date($date_format,strtotime(str_replace('/','-',trim($deadlinepath->plaintext)))); # Don't use slashes -- US dateformat assumed if you do
+
+    $application = array(
+        'council_reference' => $council_reference,
+        'address' => $address,
+        'lat' => $lat,
+        'lng' => $lng,
+        'description' => $description,
+        'info_url' => $info_url,
+        'comment_url' => $comment_url,
+        'date_scraped' => $date_scraped,
+        'date_received' => $date_received,
+        'on_notice_from' => $on_notice_from,
+        'on_notice_to' => $on_notice_to
+    );
+    
+    print_r($application);
+    die();
 }
-#print_r($applications);
+
 
 //
 // // Write out to the sqlite database using scraperwiki library
